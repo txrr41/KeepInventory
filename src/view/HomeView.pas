d72579f1fbb47852912843bbb22b2ed0b8a15c65
unknown <kauantxr41@gmail.com>
2025-09-20 22:32:03 -0300

    💄 feat: ajustando interface do menu lateral #4

--- src/view/HomeView.pas
+++ src/view/HomeView.pas
@@ -4,13 +4,31 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Skia;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Skia, Vcl.StdCtrls,
+  Vcl.ComCtrls;
 
 type
   TFormHome = class(TForm)
     Panel1: TPanel;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image6: TImage;
+    Image5: TImage;
     Image1: TImage;
+    Image7: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Image8: TImage;
+    Label7: TLabel;
+    Label8: TLabel;
+    Panel2: TPanel;
     procedure Image1Click(Sender: TObject);
+
   private
     { Private declarations }
   public
@@ -24,20 +42,22 @@ implementation
 
 {$R *.dfm}
 
+
+
+
 procedure TFormHome.Image1Click(Sender: TObject);
 begin
+
+
    if Panel1.Width = 49 then begin
-   Panel1.Color := clbtnface;
-   Panel1.Width := 161;
-   end else if Panel1.Width = 161 then begin
-   Panel1.Color := clsilver;
+
+   Panel1.Width := 177;
+   end else if Panel1.Width = 177 then begin
+
    Panel1.Width := 49;
    end;
 
-
-
-
-
 end;
+
 
 end.
